99e82b9733471ab38d0f3b9205e59216102e5955
成都-方欣芮 <sssrrr@vip.qq.com>
2018-08-23 17:50:25 +0800

    :sparkles: npm i normalize.css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <bi-button></bi-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'normalize.css';
+import biButton from './components/button';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    biButton
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
