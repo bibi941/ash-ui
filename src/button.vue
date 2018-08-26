9bd7c03fdc7ca904bc4330117a293f8dcd512936
成都-方欣芮 <sssrrr@vip.qq.com>
2018-08-26 16:22:17 +0800

    :sparkles: 验证iconPosition为left或者right

--- src/button.vue
+++ src/button.vue
@@ -10,24 +10,56 @@
 </template>
 <script>
   export default {
-    props: ['icon', 'iconPosition']
+    // props: ['icon', 'iconPosition']
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(val) {
+          return !(val !== 'left' && val !== 'right');
+        }
+      }
+    }
   }
 </script>
 <style lang="scss">
-  .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
-    border-radius: var(--border-radius); border: 1px solid var(--border-color);
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
     background: var(--button-bg);
-    display: inline-flex; justify-content: center; align-items: center;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
     vertical-align: middle;
-    &:hover { border-color: var(--border-color-hover); }
-    &:active { background-color: var(--button-active-bg); }
-    &:focus { outline: none; }
-    > .content { order: 2; }
-    > .icon { order: 1; margin-right: .1em; }
-
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+    > .content {
+      order: 2;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
     &.icon-right {
-      > .content { order: 1; }
-      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+      > .content {
+        order: 1;
+      }
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
     }
   }
 </style>
