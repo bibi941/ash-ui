8572081af90b43bd95096795ddf102a6171163a5
成都-方欣芮 <sssrrr@vip.qq.com>
2018-09-27 23:37:32 +0800

    :spanrkles: 添加row组件gutter间隔、添加col组件offset偏移

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`span-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `span-${span}`,offset && `offset-${offset}`]"
+       :style="{paddingLeft:gutter/2+`px`,paddingRight:gutter/2+`px`}">
+    <div style="border: 1px solid black">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,7 +13,15 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
       }
+    },
+    data() {
+      return {
+        gutter: 0
+      };
     }
   };
 </script>
@@ -18,13 +29,19 @@
 <style scoped lang='scss' type="text/scss">
   .col {
     height: 100px;
-    background: gainsboro;
+    /*background: gainsboro;*/
     width: 50%;
-    border: 1px solid red;
+    /*border: 1px solid red;*/
     $class-prefix: span-;
     @for $i from 1 through 24 {
       &.#{$class-prefix}#{$i} {
         width: ($i / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2+`px`,marginRight:-gutter/2+`px`}">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'ash-row'
+    name: 'ash-row',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted() {
+      console.log(this.$children);
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter;
+      });
+    }
   };
 </script>
 
