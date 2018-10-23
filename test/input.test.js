3c7992d375a582a47dd927489661fb5eadd983ce
成都-方欣芮 <sssrrr@vip.qq.com>
2018-10-23 18:00:44 +0800

    :whie_check_mark: 添加 toast 测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -1,91 +1,88 @@
-const expect = chai.expect;
-import Vue from 'vue';
-import Input from '../src/input';
+const expect = chai.expect
+import Vue from 'vue'
+import Input from '../src/input'
 
-Vue.config.productionTip = false;
-Vue.config.devtools = false;
+Vue.config.productionTip = false
+Vue.config.devtools = false
 
 describe('Input', () => {
   it('存在.', () => {
-    expect(Input).to.exist;
-  });
+    expect(Input).to.exist
+  })
 
   describe('props', () => {
-    const Constructor = Vue.extend(Input);
-    let vm;
+    const Constructor = Vue.extend(Input)
+    let vm
     afterEach(() => {
-      vm.$destroy();
-    });
+      vm.$destroy()
+    })
 
     it('接受value', () => {
       vm = new Constructor({
         propsData: {
           value: '1234'
         }
-      }).$mount();
-      const inputElement = vm.$el.querySelector('input');
-      // console.log(inputElement);
-      expect(inputElement.value).to.equal('1234');
-      vm.$destroy();
-    });
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.value).to.equal('1234')
+      vm.$destroy()
+    })
     it('接受disabled', () => {
       vm = new Constructor({
         propsData: {
           disabled: true
         }
-      }).$mount();
-      const inputElement = vm.$el.querySelector('input');
-      // console.log(inputElement);
-      expect(inputElement.disabled).to.equal(true);
-      vm.$destroy();
-    });
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.disabled).to.equal(true)
+      vm.$destroy()
+    })
     it('接受readonly', () => {
-      const Constructor = Vue.extend(Input);
+      const Constructor = Vue.extend(Input)
       vm = new Constructor({
         propsData: {
           readonly: true
         }
-      }).$mount();
-      const inputElement = vm.$el.querySelector('input');
-      // console.log(inputElement);
-      expect(inputElement.readOnly).to.equal(true);
-    });
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.readOnly).to.equal(true)
+    })
     it('接受error', () => {
-      const Constructor = Vue.extend(Input);
+      const Constructor = Vue.extend(Input)
       vm = new Constructor({
         propsData: {
           error: '你错了'
         }
-      }).$mount();
-      const useElement = vm.$el.querySelector('use');
-      expect(useElement.getAttribute('xlink:href')).to.equal('#i-error');
-      const errorMessage = vm.$el.querySelector('.error-text');
-      expect(errorMessage.innerHTML).to.equal('你错了');
-    });
-  });
+      }).$mount()
+      const useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+      const errorMessage = vm.$el.querySelector('.error-text')
+      expect(errorMessage.innerHTML).to.equal('你错了')
+    })
+  })
   describe('事件', () => {
-    const Constructor = Vue.extend(Input);
-    let vm;
+    const Constructor = Vue.extend(Input)
+    let vm
     afterEach(() => {
-      vm.$destroy();
-    });
+      vm.$destroy()
+    })
     it('支持change/input/focus/blur事件 ', () => {
       ['change', 'input', 'focus', 'blur'].map((e) => {
-        vm = new Constructor({}).$mount();
-        const callBack = sinon.fake();
-        vm.$on(e, callBack);
+        vm = new Constructor({}).$mount()
+        const callBack = sinon.fake()
+        vm.$on(e, callBack)
         //触发input事件
-        let event = new Event(e);
+        let event = new Event(e)
         Object.defineProperty(event, 'target', {
           value: {
             value: 'haha'
           }, enumerable: true
-        });
-        let inputElement = vm.$el.querySelector('input');
-        inputElement.dispatchEvent(event);
-        expect(callBack).to.have.been.calledWith('haha');
-      });
-    });
-  });
-});
+        })
+        let inputElement = vm.$el.querySelector('input')
+        inputElement.dispatchEvent(event)
+        expect(callBack).to.have.been.calledWith('haha')
+      })
+    })
+  })
+})
 
--- test/toast.test.js
+++ test/toast.test.js
@@ -10,5 +10,63 @@ describe('Toast', () => {
     expect(Toast).to.exist
   })
 
+  it('接受 autoClose', (done) => {
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Toast)
+    const vm = new Constructor({
+      propsData: {
+        autoClose: 1
+      }
+    }).$mount(div)
+    vm.$on('close', () => {
+      expect(document.body.contains(vm.$el)).to.eq(false)
+      done()
+      div.remove()
+      vm.$destroy()
+    })
+  })
+
+  it('接受 closeButton', () => {
+    const callback = sinon.fake()
+    const Constructor = Vue.extend(Toast)
+    const vm = new Constructor({
+      propsData: {
+        closeButton: {
+          text: 'ojbk',
+          callback
+        }
+      }
+    }).$mount()
+    let closeButton = vm.$el.querySelector('.button')
+    expect(closeButton.textContent.trim()).to.eq('ojbk')
+    closeButton.click()
+    expect(callback).to.have.been.called
+    vm.$destroy()
+  })
+
+  it('接受 enableHtml', () => {
+    const Constructor = Vue.extend(Toast)
+    const vm = new Constructor({
+      propsData: {
+        enableHtml: true
+      }
+    })
+    vm.$slots.default = ['<strong id="test">hi</strong>']
+    vm.$mount()
+    let strong = vm.$el.querySelector('#test')
+    expect(strong).to.exist
+    vm.$destroy()
+  })
+
+  it('接受 position', () => {
+    const Constructor = Vue.extend(Toast)
+    const vm = new Constructor({
+      propsData: {
+        position: 'bottom'
+      }
+    }).$mount()
+    expect(vm.$el.classList.contains('position-bottom')).to.eq(true)
+  })
 })
 
