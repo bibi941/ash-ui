e7458d80f61489fc658c0270e4e3fd9c0e4bbc91
成都-方欣芮 <sssrrr@vip.qq.com>
2018-11-08 21:06:43 +0800

    :hammer: 删除 app.js，入口缩放到 App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-<b-button icon="settings">啊啊啊</b-button>
+<b-button icon="settings">哥哥</b-button>
   </div>
 </template>
 
@@ -18,20 +18,45 @@ import Header from './hearder'
 import Content from './content'
 import Footer from './footer'
 import Toast from './toast'
-import plugin from './plugin'
+import plugin from './toast'
 import Tabs from './tabs'
 import TabsHead from './tabs-head'
 import TabsItem from './tabs-item'
 import TabsBody from './tabs-body'
 import TabsPane from './tabs-pane'
-import popOver from './popover'
+import PopOver from './popover'
 import Collapse from './collapse'
 import CollapseItem from './collapse-item'
 Vue.use(plugin)
+
 export default {
   name: 'app',
   components: {
-    'b-button':Button
+    'b-button':Button,
+    'b-icon':Icon,
+    'b-button-group':ButtonGroup,
+    'b-input':Input,
+    'b-row':Row,
+    'b-col':Col,
+    'b-layout':Layout,
+    'b-sider':Sider,
+    'b-Header':Header,
+    'b-content':Content,
+    'b-footer':Footer,
+    'b-toast':Toast,
+    'b-tabs':Tabs,
+    'b-tabs-head':TabsHead,
+    'b-tabs-item':TabsItem,
+    'b-tabs-body':TabsBody,
+    'b-tabs-pane':TabsPane,
+    'b-popover':PopOver,
+    'b-collapse':Collapse,
+    'b-collapse-item':CollapseItem,
+  },
+  data(){
+    return{
+
+    }
   }
 }
 </script>
