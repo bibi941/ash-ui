3fde4190d6e87d236fa2f927675050a6ab541aa1
成都-方欣芮 <sssrrr@vip.qq.com>
2018-12-23 23:16:03 +0800

    :white_check_mark: cascader test:改为Vue Test Utils进行测试,修改 svg 报错

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai'
+import Button from '@/button.vue'
+import { mount } from '@vue/test-utils'
+
+
+describe('Button', () => {
+  it('存在.', () => {
+    expect(Button).to.exist
+  })
+
+})
