c3a21661730f7d4f2cbcf072c6ac45bf1e13d9e2
成都-方欣芮 <sssrrr@vip.qq.com>
2018-12-24 00:39:10 +0800

    :white_check_mark: button test:一个 button 的测试用例已修改、修改 package json 使travis CI success

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,6 +1,11 @@
-import { expect } from 'chai'
+// noinspection ES6CheckImport
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
 import Button from '@/button.vue'
 import { mount } from '@vue/test-utils'
+
+chai.use(sinonChai)
 
 
 describe('Button', () => {
@@ -8,4 +13,65 @@ describe('Button', () => {
     expect(Button).to.exist
   })
 
+  it('可以设置icon.', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    const useElement = wrapper.find('use')
+    expect(useElement.attributes()['href']).to.equal('#i-settings')
+  })
+
+  it('可以设置loading.', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings',
+        loading:true
+      }
+    })
+    const vm =wrapper.vm
+    const useElements = vm.$el.querySelectorAll('use')
+    expect(useElements.length).to.equal(1)
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+    vm.$destroy()
+  })
+
+  xit('icon 默认的 order 是 1', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings'
+      }
+    }).$mount(div)
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('1')
+  })
+
+  xit('设置 iconPosition 可以改变 order', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    })
+    const vm =wrapper.vm
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+  })
+
+  it('点击 button 触发 click 事件', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'settings'
+      }
+    })
+    const vm =wrapper.vm
+    const callback = sinon.fake()
+    vm.$on('click', callback)
+    vm.$el.click()
+    expect(callback).to.have.been.called
+  })
 })
