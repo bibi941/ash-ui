c309f8ecde60e9808b4b5c5463ed7472f3ed6570
成都-方欣芮 <sssrrr@vip.qq.com>
2019-01-03 22:16:16 +0800

    :bug: popover  fix:销毁监听器

--- src/popover.vue
+++ src/popover.vue
@@ -41,7 +41,7 @@
         this.$refs.popover.addEventListener('mouseleave', this.close)
       }
     },
-    destroyed () {
+    beforeDestroy () {
       if (this.trigger === 'click') {
         this.$refs.popover.removeEventListener('click', this.onclick)
       } else {
