cc636da0e548d6006537c04d0a18a76cfd77cf14
成都-方欣芮 <sssrrr@vip.qq.com>
2019-01-06 22:32:02 +0800

    :sparkles: nav feat:创建nav 组件

---
+++ src/nav/nav.vue
@@ -0,0 +1,27 @@
+/**
+* @create-date :2019-Jan-Sunday-10:30 PM
+* @author : fangXinRui
+*/
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'ash-nav',
+    props: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style scoped lang='scss' type="text/scss">
+
+</style>
---
+++ src/nav/sub-nav.vue
@@ -0,0 +1,27 @@
+/**
+* @create-date :2019-Jan-Sunday-10:31 PM
+* @author : fangXinRui
+*/
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'ash-sub-nav',
+    props: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style scoped lang='scss' type="text/scss">
+
+</style>
