73ffddb85fb4aeb3f048ac5899093010ea62e1d7
成都-方欣芮 <sssrrr@vip.qq.com>
2019-01-19 22:50:33 +0800

    :hammer: :white_check_mark:  test: 才用 karma 作为新的 test runner、改写 test 测试用例

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,16 +1,14 @@
-import chai, { expect } from 'chai'
+import chai, {expect} from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import Button from "../../src/button/button.vue"
-import { mount } from '@vue/test-utils'
+import {shallowMount, mount} from '@vue/test-utils'
+import Button from '../../src/button/button'
 chai.use(sinonChai)
 
-
-describe('Button', () => {
+describe('Button.vue', () => {
   it('存在.', () => {
     expect(Button).to.exist
   })
-
   it('可以设置icon.', () => {
     const wrapper = mount(Button, {
       propsData: {
@@ -20,56 +18,56 @@ describe('Button', () => {
     const useElement = wrapper.find('use')
     expect(useElement.attributes()['href']).to.equal('#i-settings')
   })
-
   it('可以设置loading.', () => {
     const wrapper = mount(Button, {
       propsData: {
         icon: 'settings',
-        loading:true
+        loading: true
       }
     })
-    const vm =wrapper.vm
+    const vm = wrapper.vm
     const useElements = vm.$el.querySelectorAll('use')
     expect(useElements.length).to.equal(1)
     expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
-    vm.$destroy()
   })
 
-  xit('icon 默认的 order 是 1', () => {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
+  it('icon 默认的 order 是 1', () => {
+    const wrapper = mount(Button, {
+      attachToDocument: true,
       propsData: {
-        icon: 'settings'
+        icon: 'settings',
       }
-    }).$mount(div)
+    })
+    const vm = wrapper.vm
     const icon = vm.$el.querySelector('svg')
     expect(getComputedStyle(icon).order).to.eq('1')
   })
 
-  xit('设置 iconPosition 可以改变 order', () => {
+  it('设置 iconPosition 可以改变 order', () => {
     const wrapper = mount(Button, {
+      attachToDocument: true,
       propsData: {
         icon: 'settings',
         iconPosition: 'right'
       }
     })
-    const vm =wrapper.vm
+    const vm = wrapper.vm
     const icon = vm.$el.querySelector('svg')
     expect(getComputedStyle(icon).order).to.eq('2')
   })
-
   it('点击 button 触发 click 事件', () => {
     const wrapper = mount(Button, {
       propsData: {
-        icon: 'settings'
+        icon: 'settings',
       }
     })
-    const vm =wrapper.vm
-    const callback = sinon.fake()
+    const vm = wrapper.vm
+
+
+    const callback = sinon.fake();
     vm.$on('click', callback)
     vm.$el.click()
     expect(callback).to.have.been.called
+
   })
 })
