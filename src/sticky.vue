5004269210a1e449a5d159cc6b1d872f96f555bf
成都-方欣芮 <sssrrr@vip.qq.com>
2019-01-31 20:04:48 +0800

    :sparkles: sticky feat: 创建 sticky 组件

---
+++ src/sticky.vue
@@ -0,0 +1,27 @@
+/**
+* @create-date :2019-Jan-Thursday-10:20 AM
+* @author : fangXinRui
+*/
+<template>
+  <div class="ash-sticky"></div>
+</template>
+
+<script>
+  export default {
+    name: 'ash-sticky',
+    props: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style scoped lang='scss' type="text/scss">
+
+</style>
