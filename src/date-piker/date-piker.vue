9fe4d07538e42627865730b0fbe5a0a292d114f0
成都-方欣芮 <sssrrr@vip.qq.com>
2019-03-05 23:17:32 +0800

    :sparkles: date-piker feat: 创建日期选择器(date-piker)组件

---
+++ src/date-piker/date-piker.vue
@@ -0,0 +1,29 @@
+/**
+* @create-date :2019-Mar-Tuesday-11:11 PM
+* @author : fangXinRui
+*/
+<template>
+  <div>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ash-date-piker',
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped lang='scss' type="text/scss">
+
+</style>
